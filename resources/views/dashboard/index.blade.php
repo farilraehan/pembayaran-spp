ca00ea66480d4b3573029061f8b5b6bc31aaab87
nurfandi31 <fandinur241@gmail.com>
2025-12-01 11:25:21 +0700

    test

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,7 +4,7 @@
         <div class="ms-3">
             <h3 class="mb-0 h4 font-weight-bolder">Dashboard</h3>
             <p class="mb-4">
-                Check the sales, value and bounce rate by test.
+                Check the sales, value and bounce rate test.
             </p>
         </div>
         <div class="row mb-4">
