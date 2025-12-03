95f3cc610946ecb0a46e25e8f32be5fb22f768f8
nurfandi31 <fandinur241@gmail.com>
2025-12-03 10:10:50 +0700

    title

--- resources/views/laporan_keuangan/index.blade.php
+++ resources/views/laporan_keuangan/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.base')
 @section('content')
     <div class="row">
+        <div class="ms-3">
+            <h3 class="mb-0 h4 font-weight-bolder">Laporan Keuangan</h3>
+        </div>
         <div class="col-12">
             <div class="card my-4">
                 <div class="card-body px-0 pb-2">
