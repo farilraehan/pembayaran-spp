54a51fd6b2c58c982be34098b7309676f8d137d2
nurfandi31 <fandinur241@gmail.com>
2025-12-04 08:45:45 +0700

    template dashboard

--- resources/views/jenis_keuangan/index.blade.php
+++ resources/views/jenis_keuangan/index.blade.php
@@ -1,24 +1,18 @@
 @extends('layouts.base')
 @section('content')
     <div class="row">
-        <div class="ms-3">
-            <h3 class="mb-0 h4 font-weight-bolder">Jenis Keuangan</h3>
-        </div>
         <div class="col-12">
             <div class="card my-4">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                     <div
-                        class="bg-gradient-light shadow-secondary border-radius-lg pt-3 pb-1 d-flex justify-content-between align-items-center">
-                        <h6 class="text-white text-capitalize ps-3">Authors table</h6>
+                        class="bg-gradient-dark shadow-secondary border-radius-lg pt-3 pb-1 d-flex justify-content-between align-items-center">
+                        <h6 class="text-white text-capitalize ps-3">Jenis Keuangan</h6>
                         <div class="pe-3">
                             <a href="#" class="btn btn-sm btn-light text-dark">
                                 + Tambah Data
                             </a>
                             <a href="#" class="btn btn-sm btn-info text-dark">
                                 Lihat
-                            </a>
-                            <a href="#" class="btn btn-sm btn-primary text-dark">
-                                Tambah Jenis Pembayaran
                             </a>
                         </div>
                     </div>
