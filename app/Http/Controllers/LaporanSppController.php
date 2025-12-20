f5b0bbfba81108b3919772a5db9fa4d894af9cbc
farilraehan <farilraihan50@gmail.com>
2025-12-20 09:33:16 +0700

    laporan pembayaran spp

--- app/Http/Controllers/LaporanSppController.php
+++ app/Http/Controllers/LaporanSppController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\Models\Tahun_Akademik;
 use App\Models\Kelas;
+use App\Models\Spp;
+use App\Models\Anggota_Kelas;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class LaporanSppController extends Controller
@@ -67,18 +69,81 @@ class LaporanSppController extends Controller
 
     private function spp(array $data)
     {
+        $data['title'] = 'Laporan Pembayaran SPP';
+
+        $data['kelas'] = !empty($data['kelas_id'])
+            ? Kelas::find($data['kelas_id'])
+            : null;
+
+        $data['periode'] = [
+            'awal'  => Carbon::parse($data['tgl_awal'])->locale('id'),
+            'akhir' => Carbon::parse($data['tgl_akhir'])->locale('id'),
+        ];
+
+        $anggotaKelas = Anggota_Kelas::with(['getSiswa', 'getTahunAkademik'])
+        ->where('status', 'aktif')
+        ->when(!empty($data['kelas_id']), function ($q) use ($data) {
+            $kelas = Kelas::find($data['kelas_id']);
+            if ($kelas) {
+                $q->where('kode_kelas', $kelas->kode_kelas);
+            }
+        })
+        ->when(!empty($data['tahun_akademik_id']), function ($q) use ($data) {
+            $tahun = Tahun_Akademik::find($data['tahun_akademik_id']);
+            if ($tahun) {
+                $q->where('tahun_akademik', $tahun->nama_tahun);
+            }
+        })
+        ->orderBy('id')
+        ->get()
+        ->map(function ($row) use ($data) {
+
+            $tglAwal  = Carbon::parse($data['tgl_awal'])->startOfMonth();
+            $tglAkhir = Carbon::parse($data['tgl_akhir'])->endOfMonth();
+
+            // jumlah bulan periode
+            $jumlahBulan = $tglAwal->diffInMonths($tglAkhir) + 1;
+
+            // SPP per bulan
+            $row->per_bulan = $row->getSiswa->spp_nominal ?? 0;
+
+            // TARGET s.d saat ini
+            $row->target_sd_saat_ini = $jumlahBulan * $row->per_bulan;
+
+            // REALISASI s.d periode lalu (yg LUNAS)
+            $row->sd_periode_lalu = Spp::where('anggota_kelas', $row->id)
+                ->where('status', 'L')
+                ->where('tanggal', '<', $tglAwal)
+                ->sum('nominal');
+
+            // REALISASI periode ini (yg LUNAS)
+            $row->periode_ini = Spp::where('anggota_kelas', $row->id)
+                ->where('status', 'L')
+                ->whereBetween('tanggal', [$tglAwal, $tglAkhir])
+                ->sum('nominal');
+
+            // TOTAL REALISASI
+            $row->sd_periode_ini = $row->sd_periode_lalu + $row->periode_ini;
+
+            // SISA / TUNGGAKAN
+            $row->sisa = $row->target_sd_saat_ini - $row->sd_periode_ini;
+
+            return $row;
+        });
+
+        $data['anggotaKelas'] = $anggotaKelas;
         $pdf = Pdf::loadView('laporan_spp.views.pembayaran_spp', $data)
+            ->setPaper('A4', 'landscape')
             ->setOptions([
                 'margin-top'    => 30,
                 'margin-bottom' => 15,
-                'margin-left'   => 25,
+                'margin-left'   => 20,
                 'margin-right'  => 20,
                 'enable-local-file-access' => true,
             ]);
 
         return $pdf->stream('laporan-spp.pdf');
     }
-
 
     private function daftar_ulang(array $data)
     {
--- resources/views/laporan_spp/views/pembayaran_spp.blade.php
+++ resources/views/laporan_spp/views/pembayaran_spp.blade.php
@@ -1,1 +1,114 @@
-ddddd
+<title>{{ $title }}</title>
+@extends('laporan.layout.base')
+
+@section('content')
+    <table width="100%" cellspacing="0" cellpadding="0" style="margin-bottom:15px;">
+        <tr>
+            <td align="center" style="padding:0;">
+                <div style="font-size:20px; font-weight:bold; margin:0; padding:0; line-height:1.1;">
+                    {{ $title }}
+                    @if (!empty($kelas))
+                        Kelas {{ $kelas->kode_kelas }}
+                    @endif
+                </div>
+
+                <div style="font-size:16px; font-weight:bold; margin:2px 0 0 0; padding:0; line-height:1.1;">
+                    Periode
+                    {{ $periode['awal']->translatedFormat('d F Y') }}
+                    s.d.
+                    {{ $periode['akhir']->translatedFormat('d F Y') }}
+                </div>
+            </td>
+
+        </tr>
+    </table>
+
+    <table width="100%" cellpadding="4" cellspacing="0" style="border-collapse:collapse; font-size:11px;">
+
+        <thead>
+            <tr style="text-align:center; font-weight:bold;">
+                <th rowspan="2" style="border:1px solid #000; width:4%;">No</th>
+                <th rowspan="2" style="border:1px solid #000; width:10%;">NISN</th>
+                <th rowspan="2" style="border:1px solid #000; width:20%;">Nama Siswa</th>
+
+                <th colspan="2" style="border:1px solid #000; width:16%;">
+                    Target Pembayaran
+                </th>
+
+                <th colspan="3" style="border:1px solid #000; width:30%;">
+                    Realisasi Pembayaran
+                </th>
+
+                <th rowspan="2" style="border:1px solid #000; width:20%;">Keterangan</th>
+            </tr>
+
+            <tr style="text-align:center; font-weight:bold;">
+                <th style="border:1px solid #000; width:12%;">per Bulan</th>
+                <th style="border:1px solid #000; width:12%;">s.d. Saat Ini</th>
+
+                <th style="border:1px solid #000; width:12%;">s.d. Periode Lalu</th>
+                <th style="border:1px solid #000; width:12%;">Periode Ini</th>
+                <th style="border:1px solid #000; width:12%;">s.d. Periode Ini</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($anggotaKelas as $i => $row)
+                <tr>
+                    <td style="border:1px solid #000; text-align:center;">{{ $i + 1 }}</td>
+
+                    <td style="border:1px solid #000; text-align:center;">
+                        {{ $row->getSiswa->nisn ?? '-' }}
+                    </td>
+
+                    <td style="border:1px solid #000;">
+                        {{ $row->getSiswa->nama ?? '-' }}
+                    </td>
+
+                    {{-- Target per bulan --}}
+                    <td style="border:1px solid #000; text-align:right;">
+                        {{ number_format($row->per_bulan, 2, ',', '.') }}
+                    </td>
+
+                    {{-- Target s.d saat ini --}}
+                    <td style="border:1px solid #000; text-align:right;">
+                        {{ number_format($row->target_sd_saat_ini, 2, ',', '.') }}
+                    </td>
+
+                    {{-- Realisasi s.d periode lalu --}}
+                    <td style="border:1px solid #000; text-align:right;">
+                        {{ number_format($row->sd_periode_lalu, 2, ',', '.') }}
+                    </td>
+
+                    {{-- Realisasi periode ini --}}
+                    <td style="border:1px solid #000; text-align:right;">
+                        {{ number_format($row->periode_ini, 2, ',', '.') }}
+                    </td>
+
+                    {{-- Realisasi s.d periode ini --}}
+                    <td style="border:1px solid #000; text-align:right;">
+                        {{ number_format($row->sd_periode_ini, 2, ',', '.') }}
+                    </td>
+
+                    {{-- Keterangan --}}
+                    <td
+                        style="border:1px solid #000; text-align:center;
+    color: {{ $row->sisa > 0 ? 'red' : 'black' }};">
+                        @if ($row->sisa > 0)
+                            ({{ number_format($row->sisa, 2, ',', '.') }})
+                        @else
+                            Lunas
+                        @endif
+                    </td>
+
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="9" style="border:1px solid #000; text-align:center; font-style:italic;">
+                        Tidak ada data
+                    </td>
+                </tr>
+            @endforelse
+        </tbody>
+
+    </table>
+@endsection
