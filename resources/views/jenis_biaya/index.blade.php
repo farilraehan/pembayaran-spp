a93ed279cc804ecb94d0fbd63647cedf91e3ad01
nurfandi31 <fandinur241@gmail.com>
2025-12-24 11:05:59 +0700

    replay

--- resources/views/jenis_biaya/index.blade.php
+++ resources/views/jenis_biaya/index.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.base')
 @section('content')
     <div class="row">
-
         <div class="col-12">
             <div class="card my-4">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                     <div
-                        class="bg-gradient-dark shadow-secondary border-radius-lg pt-3 pb-1 d-flex justify-content-between align-items-center">
+                        class="bg-gradient-secondary shadow-secondary border-radius-lg pt-3 pb-1 d-flex justify-content-between align-items-center">
                         <h6 class="text-white text-capitalize ps-3" id="headerTitle">
                             Jenis dan Nominal Pembayaran <span id="titleYear" class="text-muted"></span>
                         </h6>
